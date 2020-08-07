44d5742920f8f26bd4be972a33f7a6cea9d787fe
RobertoLopez <robertolopez201930351@cunoc.edu.gt>
2020-08-07 01:10:35 -0600

    se realizo un cambio bro

--- src/proyectoprueba/ProyectoPrueba.java
+++ src/proyectoprueba/ProyectoPrueba.java
@@ -15,7 +15,8 @@ public class ProyectoPrueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int n=1;
+        System.out.println("hola"+n);
     }
     
 }
